37859dcf5bb8ecb47d71c9cab563a88c1a9a8d69
yuedan <danyue9528@163.com>
2022-07-01 16:09:03 +0800

    提交

--- MyApplication/app/src/main/java/com/ybs/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/ybs/myapplication/MainActivity.kt
@@ -1,59 +1,66 @@
 package com.ybs.myapplication
 
-import android.content.ComponentName
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
-import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import me.xfans.lib.voicewaveview.VoiceWaveView
 
 
 class MainActivity : AppCompatActivity() {
-    val tags = arrayOf(
-        "com.hdyj.tooltest",
-        "com.hdyj.settingmodule.ui.activity.NoticeActivity",
-        "com.hdyj.tooltest.setting.NOTICE",
-        "com.hdyj.settingmodule",
-    )
-
+    val REQUEST_CODE = 11
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val voiceWaveView: VoiceWaveView = findViewById(R.id.voiceWaveView0)
-        voiceWaveView.post {
-            val unitWidth: Float = voiceWaveView.measuredWidth / 1052f
-            voiceWaveView.apply {
-                lineWidth = unitWidth * 20
-                lineSpace = unitWidth * 66
-                addBody(14)
-                addBody(25)
-                addBody(31)
-                addBody(48)
-                addBody(49)
-                addBody(43)
-                addBody(81)
-                addBody(43)
-                addBody(49)
-                addBody(49)
-                addBody(31)
-                addBody(25)
-                addBody(14)
-                start()
+        findViewById<RecorderCrop>(R.id.crop).setClick { b, s, e ->
+            val str = "$s - $e"
+            if (b) {
+                Toast.makeText(this, "保留 - $str", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "删除 - $str", Toast.LENGTH_LONG).show()
             }
         }
-        findViewById<Button>(R.id.notice_btn).setOnClickListener {
-            var t = findViewById<EditText>(R.id.notice_edt).text.toString()
-            if (t.isEmpty()) {
-                t = "1"
-            }
-            val intent = Intent()
-            val comp = ComponentName(tags[0], tags[1])
-            intent.component = comp
-            intent.putExtra("notice_type", t.toInt())
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            intent.action = tags[2]
-            startActivity(intent)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            checkSetting()
         }
+    }
+
+
+    private fun checkSetting() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "当前无权限，请授权", Toast.LENGTH_SHORT).show()
+                startActivityForResult(
+                    Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:$packageName")
+                    ), REQUEST_CODE
+                )
+            } else {
+                show()
+            }
+        } else {
+            show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show()
+                } else {
+                    show()
+                }
+            }
+        }
+    }
+
+    private fun show() {
+        MyWindowManager.createSmallWindow(this)
     }
 }
